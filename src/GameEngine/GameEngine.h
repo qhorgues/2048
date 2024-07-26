6f36111e4a3323d044ff10ee8c6d458ae7126e55
Githonil <anthony.gimenez06@gmail.com>
2024-07-26 16:44:55 +0200

    Correction spawnRandomNumber, the merge and add the score, and the condition if the player lose

---
+++ src/GameEngine/GameEngine.h
@@ -0,0 +1,80 @@
+#ifndef GAME_ENGINE_H
+#define GAME_ENGINE_H
+
+#include <stdbool.h>
+
+/**
+ * @brief Cette structure représente le jeu 2048.
+ */
+struct GameEngine
+{
+  int array[4][4];
+  int score;
+};
+
+
+
+/**
+ * @brief Initialise le jeu du 2048.
+ * 
+ * @return struct GameEngine - Renvoie le jeu du 2048.
+ */
+struct GameEngine initGameEngine(void);
+
+
+
+/**
+ * @brief Fait apparaître un nombre dans le jeu.
+ * 
+ * @param[in, out] gameEngine Le jeu du 2048.
+ */
+void spawnRandomNumber(struct GameEngine * gameEngine);
+
+
+
+/**
+ * @brief Fait le déplacement du haut.
+ * 
+ * @param[in, out] gameEngine Le jeu du 2048.
+ */
+void moveUp(struct GameEngine * gameEngine);
+
+
+
+/**
+ * @brief Fait le déplacement du bas.
+ * 
+ * @param[in, out] gameEngine Le jeu du 2048.
+ */
+void moveDown(struct GameEngine * gameEngine);
+
+
+
+/**
+ * @brief Fait le déplacement à gauche.
+ * 
+ * @param[in, out] gameEngine Le jeu du 2048.
+ */
+void moveLeft(struct GameEngine * gameEngine);
+
+
+
+/**
+ * @brief Fait le jeu à droite.
+ * 
+ * @param[in, out] gameEngine Le jeu du 2048.
+ */
+void moveRight(struct GameEngine * gameEngine);
+
+
+
+/**
+ * @brief Regarde si le jeu est fini.
+ * 
+ * @param[in] gameEngine Le jeu du 2048.
+ * 
+ * @return bool - Renvoie false si le jeu n'est pas fini, true sinon.
+ */
+bool isEnding(struct GameEngine * gameEngine);
+
+#endif
