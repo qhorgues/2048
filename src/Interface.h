b02b6cd16d5124f5f8dff9cd9e47ccefd95e58c6
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-27 13:30:37 +0200

    modification avec le game engine

--- src/Interface.h
+++ src/Interface.h
@@ -2,13 +2,13 @@
 #define INTERFACE_H
 
 #include "GameStatus.h"
-#include "Board/Board.h"
+#include "GameEngine/GameEngine.h"
 #include "Intercations.h"
 
 typedef void Interface;
 
 Interface* initInterface(char const* dir_exe);
-void update(Interface* interface, enum GameStatus status, struct Board const* board);
+void update(Interface* interface, enum GameStatus status, struct GameEngine const* gameEngine);
 enum Interactions getInteraction(Interface* interface, enum GameStatus* status);
 void freeInterface(Interface* interface);
 
