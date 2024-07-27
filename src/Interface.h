a4b92371193fedc51bae7f0f0f298796f63d7b3e
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-27 15:43:46 +0200

    correction orthographe

--- src/Interface.h
+++ src/Interface.h
@@ -2,14 +2,14 @@
 #define INTERFACE_H
 
 #include "GameStatus.h"
-#include "Board/Board.h"
-#include "Intercations.h"
+#include "GameEngine/GameEngine.h"
+#include "Interactions.h"
 
 typedef void Interface;
 
-Interface* initInterface(void);
-void update(Interface* interface, enum GameStatus status, struct Board const* board);
-enum Interactions getInteraction(Interface* interface, enum GameStatus* status);
+Interface* initInterface(char const* dir_exe);
+void update(Interface* interface, enum GameStatus status, struct GameEngine const* gameEngine);
+enum Interactions getInteraction(Interface* interface, enum GameStatus* status, struct GameEngine const* gameEngine);
 void freeInterface(Interface* interface);
 
 #endif
