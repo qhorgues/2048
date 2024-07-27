582c4070b379c97f46c937cc63bdecf9052e4f79
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-27 16:17:05 +0200

    ajout fonction run

---
+++ src/Game/Game.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+
+#include "Interface.h"
+#include "GameEngine/GameEngine.h"
+
+void run(char const* current_dir)
+{
+    struct GameEngine engine = initGameEngine();
+    enum GameStatus gameStatus = IN_GAME;
+    enum Interactions interaction;
+    Interface* interface = initInterface(current_dir);
+    update(interface, gameStatus, &engine);
+    while (true)
+    {
+        interaction = getInteraction(interface, &gameStatus, &engine);
+        if (interaction == INTERACTION_QUIT)
+        {
+            break;
+        }
+        switch (gameStatus)
+        {
+        case IN_GAME:
+            move(&engine, interaction);
+            update(interface, gameStatus, &engine);
+            break;
+        default:
+            break;
+        }
+    }
+    freeInterface(interface);
+}
