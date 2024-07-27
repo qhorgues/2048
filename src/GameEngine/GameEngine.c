8120fc9432fc273d6bd98d744633048dfcc509a7
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-27 16:17:51 +0200

    changement pour un memset

--- src/GameEngine/GameEngine.c
+++ src/GameEngine/GameEngine.c
@@ -14,13 +14,8 @@ struct GameEngine initGameEngine(void)
 {
     struct GameEngine gameEngine;
     srand((unsigned int)time(NULL));
-    for (int x = 0; x < 4; x++)
-    {
-        for (int y = 0; y < 4; y++)
-        {
-            gameEngine.board[y][x] = 0;
-        }
-    }
+    
+    memset(gameEngine.board, 0, sizeof(int) * 4 * 4);
 
     gameEngine.score = 0;
 
