cabc664a6dbd6a6dcdaf98919f1d61bcc263b819
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-27 22:34:03 +0200

    ajout logo & bouton

--- src/Interface_SDL2/Interface_SDL2.c
+++ src/Interface_SDL2/Interface_SDL2.c
@@ -12,7 +12,10 @@
 #define SIZE_BOARD ((SIZE_TILE * 4) + (SPACE_BETWEEN_TILE * 5))
 #define RADIUS_TILE (SIZE_TILE / 10)
 #define RADIUS_BOARD RADIUS_TILE
+#define RADIUS_BUTTON RADIUS_TILE
 #define MARGIN_TOP_WITH_BOARD (WINDOW_HEIGHT - SIZE_BOARD - MARGIN_WITH_BOARD)
+#define MARGIN_LOGO 5
+#define SIZE_LOGO 90
 
 #define WHITE ((SDL_Color){255, 255, 255, SDL_ALPHA_OPAQUE})
 #define TILE_2_4_TEXT_COLOR ((SDL_Color){119, 110, 101, SDL_ALPHA_OPAQUE})
@@ -41,12 +44,65 @@ struct Interface_SDL2
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
-    TTF_Font *title_font;
+    SDL_Texture* text_logo;
     TTF_Font *button_font;
     TTF_Font *number_font[5];
     SDL_Texture *texture_number[16];
     bool key_pressed[4];
 };
+
+struct Button
+{
+    SDL_Texture* texture_button;
+    SDL_Rect rect;
+    SDL_Color color_button;
+};
+
+static struct Button* initButton(SDL_Renderer* renderer, SDL_Rect const* position, char const* text, TTF_Font* font, SDL_Color text_color, SDL_Color color_button)
+{
+    struct Button* button = malloc(sizeof(struct Button));
+    if (button == NULL)
+    {
+        return NULL;
+    }
+    SDL_Surface *surface_txt = TTF_RenderText_Shaded(font, text, text_color, color_button);
+    if (surface_txt == NULL)
+    {
+        printf("%s\n", TTF_GetError());
+        free(button);
+        return NULL;
+    }
+    button->texture_button = SDL_CreateTextureFromSurface(renderer, surface_txt);
+    SDL_FreeSurface(surface_txt);
+    if (button->texture_button == NULL)
+    {
+        printf("%s\n", TTF_GetError());
+        free(button);
+        return NULL;
+    }
+    button->rect = *position;
+    button->color_button = color_button;
+    return button;
+}
+
+static void freeButton(struct Button* button)
+{
+    SDL_DestroyTexture(button->texture_button);
+    free(button);
+}
+
+static bool checkIfPressedButton(struct Button* button, int x, int y)
+{
+    return x >= button->rect.x && x <= button->rect.x + button->rect.w
+           && y >= button->rect.y && y <= button->rect.y + button->rect.h;
+}
+
+static void RenderDrawTextRoundedRectBox_with_texture(SDL_Renderer *renderer, SDL_Rect const *rect, int radius, SDL_Texture *texture_text, SDL_Color background);
+
+static void renderButton(SDL_Renderer *renderer, struct Button * button)
+{
+    RenderDrawTextRoundedRectBox_with_texture(renderer, &button->rect, RADIUS_BUTTON, button->texture_button, button->color_button);
+}
 
 Interface *initInterface(char const *dir_exe)
 {
@@ -173,13 +229,18 @@ static SDL_Texture *RenderDrawTextRoundedRectBox(
         return NULL;
     }
     SDL_Texture *texture_text = SDL_CreateTextureFromSurface(renderer, surface_txt);
+    SDL_FreeSurface(surface_txt);
+    if (texture_text == NULL)
+    {
+        printf("%s\n", TTF_GetError());
+        return NULL;
+    }
     SDL_Rect txt_rect;
     SDL_QueryTexture(texture_text, NULL, NULL, &(txt_rect.w), &(txt_rect.h));
     txt_rect.x = rect->x + rect->w / 2 - txt_rect.w / 2;
     txt_rect.y = rect->y + rect->h / 2 - txt_rect.h / 2 + 3;
 
     SDL_RenderCopy(renderer, texture_text, NULL, &txt_rect);
-    SDL_FreeSurface(surface_txt);
     return texture_text;
 }
 
@@ -226,8 +287,8 @@ static void DrawTile(struct Interface_SDL2 *interface, int tile, int index_x, in
 
     int number_tile = 1 << tile;
     int numberChr = (int)log10(number_tile);
-    char int_to_chr[7];
-    snprintf(int_to_chr, 6, "%d", number_tile);
+    char int_to_chr[12];
+    snprintf(int_to_chr, 12, "%d", number_tile);
 
     if (interface->texture_number[tile - 1] != NULL)
     {
@@ -245,7 +306,7 @@ static void DrawTile(struct Interface_SDL2 *interface, int tile, int index_x, in
     }
 }
 
-void DrawBoard(struct Interface_SDL2 *interface, int (*board)[4])
+static void DrawBoard(struct Interface_SDL2 *interface, const int (*board)[4])
 {
     SDL_Rect rect_board = {.x = MARGIN_WITH_BOARD, .y = MARGIN_TOP_WITH_BOARD, .w = SIZE_BOARD, .h = SIZE_BOARD};
     // SDL_Rect rect_background_board = {.x = 0, .y = MARGIN_TOP_WITH_BOARD - MARGIN_WITH_BOARD, .w = WINDOW_WIDTH, .h = SIZE_BOARD + (2*MARGIN_WITH_BOARD)};
@@ -263,6 +324,38 @@ void DrawBoard(struct Interface_SDL2 *interface, int (*board)[4])
     }
 }
 
+static void DrawLogo(struct Interface_SDL2 *interface)
+{
+    if (interface->text_logo == NULL)
+    {
+        SDL_Surface *surface_txt = TTF_RenderText_Shaded(interface->number_font[2], "2048", WHITE, TILE_2048_COLOR);
+        if (surface_txt == NULL)
+        {
+            printf("%s\n", TTF_GetError());
+        }
+        interface->text_logo = SDL_CreateTextureFromSurface(interface->renderer, surface_txt);
+        SDL_FreeSurface(surface_txt);
+        if (interface->text_logo == NULL)
+        {
+            printf("%s\n", TTF_GetError());
+        }
+    }
+    SDL_Rect txt_rect;
+    SDL_Rect rect_tile = {.x = MARGIN_LOGO, .y = MARGIN_LOGO, .w = SIZE_LOGO, .h = SIZE_LOGO};
+    SDL_QueryTexture(interface->text_logo, NULL, NULL, &(txt_rect.w), &(txt_rect.h));
+    txt_rect.x =  rect_tile.x + rect_tile.w / 2 - txt_rect.w / 2;
+    txt_rect.y = rect_tile.y + rect_tile.h / 2 - txt_rect.h / 2 + 3;
+    SDL_SetRenderDrawColor(interface->renderer, TILE_2048_COLOR.r, TILE_2048_COLOR.g, TILE_2048_COLOR.b, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRoudedRect(interface->renderer, &rect_tile, RADIUS_TILE);
+    SDL_RenderCopy(interface->renderer, interface->text_logo, NULL, &txt_rect);
+}
+
+static void updateInGame(struct Interface_SDL2 *interface, struct GameEngine const *gameEngine)
+{
+    DrawLogo(interface);
+    DrawBoard(interface, gameEngine->board);
+}
+
 void update(Interface *interface, enum GameStatus status, struct GameEngine const *gameEngine)
 {
     struct Interface_SDL2 *inter = interface;
@@ -271,11 +364,12 @@ void update(Interface *interface, enum GameStatus status, struct GameEngine cons
     switch (status)
     {
     case IN_GAME:
-        DrawBoard(inter->renderer, gameEngine->board);
+        updateInGame(inter, gameEngine);
         break;
     default:
         break;
     }
+    SDL_RenderPresent(inter->renderer);
 }
 
 enum Interactions getInteraction(Interface *interface, enum GameStatus *status, struct GameEngine const *gameEngine)
@@ -381,4 +475,9 @@ void freeInterface(Interface *interface)
     free(inter);
     TTF_Quit();
     SDL_Quit();
+}
+
+void test(void)
+{
+    SDL_Delay(5000);
 }
